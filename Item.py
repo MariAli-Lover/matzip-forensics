d8f35e3f0e7f606aeee16658671a1c2216d2cd8b
강성철 <rkdtjdcjf700@gmail.com>
2017-09-22 16:14:03 +0900

    fix Item.py

--- Item.py
+++ Item.py
@@ -1,6 +1,7 @@
 class Item:
-    #버리는코드. 코딩하다보니 쓰기가애매해졌음
+    #버리는코드. 코딩하다보니 이거 쓸데가없음
     #파일하나 클래스임
+    #근데 파일하나하나 클래스에 저장할필요없이 한번 바로db에박으면 그이후로는쓸데없는듯 메모리만먹고
     def __init__(self, name, path, size, md5, sha1, modify_time, access_time, create_time, index_num, folder) :
         self.name = name
         self.path = path
