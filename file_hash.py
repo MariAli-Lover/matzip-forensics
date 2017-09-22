06e09f84d8b0749536afd121464e68fa6439d11f
강성철 <rkdtjdcjf700@gmail.com>
2017-09-22 15:59:32 +0900

    fix file_hash.py

--- file_hash.py
+++ file_hash.py
@@ -1,6 +1,6 @@
 import hashlib
 
-def sha1_for_largefile(filepath, blocksize=8192):
+def sha1_for_largefile(filepath, blocksize=8192): #sha1구하는 함수
     sha_1 = hashlib.sha1()
     try:
         f = open(filepath, "rb")
@@ -14,7 +14,7 @@ def sha1_for_largefile(filepath, blocksize=8192):
         sha_1.update(buf)
     return sha_1.hexdigest()
 
-def md5_for_largefile(filepath, blocksize=8192):
+def md5_for_largefile(filepath, blocksize=8192):#md5구하는 함수
     md5 = hashlib.md5()
     try:
         f = open(filepath, "rb")
