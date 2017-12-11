1406716de898ca32475c1bdb6cdb85ef12a88047
MariAli-Lover <humanbeing1337@gmail.com>
2017-12-11 23:12:00 +0900

    Temporary Deletion

--- globals.py
+++
@@ -1,9 +0,0 @@
-# Global하게 사용되는 변수/인스턴스들을 저장하는 공간
-# import globals as gl 형식으로 저장됨
-from dbManager import *
-
-itemList = []  # Item.py랑 같이쓰던건데 사용목적희미해짐
-default_path = ""  # explorer에서 최상위폴더 경로
-debug_mode = False  # 디버그
-
-db = None  # 데이터베이스 객체
